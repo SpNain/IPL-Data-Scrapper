1bae4ee6275da814dacb3a46b5f76f767dc0f350
HardeepNain <hnspnain@gmail.com>
2023-02-25 23:47:39 +0530

    Pivot point - v0 Org

--- allMatches.js
+++ allMatches.js
@@ -1,27 +1,28 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const getMatchDetails = require("./match");
-// const allMatchesLink = require("./homepage");  //#
 
 
-function getAllMatches(allMatchesLink){  // yaha pe isko allMatchLink joki Homepage se mila hai pass hoga
-    request(allMatchesLink , function(err , res , data){  // ye hof hai to jaise hi isko data mila ye processData ko call laga dega
+
+function getAllMatches(allMatchesLink){
+    request(allMatchesLink , function(err , res , data){
        processData(data);
     })
 }
 
 
-function processData(html){  //
+function processData(html){
     let myDocument = cheerio.load(html);
-    let allATags = myDocument('a[data-hover="Scorecard"]');  // selectors using values // jitne bhi matches us homepage pe the unke scorecard ke anker link aage
-    // console.log(allATags.length);
-    // { "0" : {aTag} , ......... "59" : {aTag} };
-
+    let allATags = myDocument('a[data-hover="Scorecard"]');
+    console.log(allATags.length);
+    //    { "0" : {aTag} , ......... "59" : {aTag} };
     for(let i=0 ; i<allATags.length ; i++){
-        let matchLink =  "https://www.espncricinfo.com" + myDocument(allATags[i]).attr("href");  // yha se hume ek ek krke saare matches ke scorecard ke link milte jaayenge 
-        // console.log(matchLink);           
-        getMatchDetails(matchLink);                                                               // jise hum yha ek ek krke pass krte jaayenge.
+        let matchLink =  "https://www.espncricinfo.com" + myDocument(allATags[i]).attr("href");
+        // console.log(matchLink);
+        getMatchDetails(matchLink);
     }   
 }
+
+
 
 module.exports = getAllMatches;
--- match.js
+++ match.js
@@ -4,31 +4,26 @@ const fs = require("fs");
 // let matchLink = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
 
 
-function getMatchDetails(matchLink){  //allMatches wale file  se iss fxn ko call lgegi //aur jo matchLink us file me prepare karke pass krenge wahi matchLink yaha pe milega
-    request(matchLink , function(error , response , data){  // jaise hi matchLink se data load ho jaayega ye cb fxn ko call lga dega
+function getMatchDetails(matchLink){
+    request(matchLink , function(error , response , data){
         processData(data);
     })
 }
 
 
-function processData(html){  // is fxn ke andar matchLink ka loaded data yani us match ki html file pass hogi 
-    
+function processData(html){
     let myDocument = cheerio.load(html);
     let bothInnings = myDocument(".card.content-block.match-scorecard-table .Collapsible");
-    
-    for (let i = 0; i < bothInnings.length; i++){
+    for(let i=0 ; i<bothInnings.length ; i++){
         let oneInning = myDocument(bothInnings[i]);
         // <div class="Collapsible"></div>
-
-        let teamName = oneInning.find("h5").text();     // yaha se hume pura naam milega
-        teamName = teamName.split("INNINGS")[0].trim();   // console se check karke ki kis basis pe hum teamName nikal skte hai us basis pe split kar diya
-        // console.log(teamName);             // trim = removes extra spaces from fornt aur end
-        
+        let teamName = oneInning.find("h5").text();
+        teamName = teamName.split("INNINGS")[0].trim();
+        console.log(teamName);
         let allTrs = oneInning.find(".table.batsman tbody tr");
-        for(let j=0 ; j<allTrs.length-1 ; j++){  // loop allTrs.length-1 tak chlaya in place of allTrs.length b/c last me extras ka ek tr hota jispe we don't want to work.
+        for(let j=0 ; j<allTrs.length-1 ; j++){
             let allTds = myDocument(allTrs[j]).find("td");
-            
-            if (allTds.length > 1) {
+            if(allTds.length > 1){
                 // batsmanName allTds[0]
                 let batsmanName = myDocument(allTds[0]).text().trim();
                 // runs allTds[2]
@@ -42,8 +37,7 @@ function processData(html){  // is fxn ke andar matchLink ka loaded data yani us
                 // sr allTds[7]
                 let strikeRate = myDocument(allTds[7]).text().trim();
                 // console.log(`Batsman = ${batsmanName} Runs = ${runs} Balls = ${balls} Fours = ${fours} Sixes = ${sixes} StrikeRate = ${strikeRate}`);
-                
-                processDetails(teamName, batsmanName, runs, balls, fours, sixes, strikeRate);
+                processDetails(teamName , batsmanName , runs , balls , fours , sixes , strikeRate);
             }
         }
     }
@@ -61,8 +55,8 @@ function checkBatsmanFile(teamName , batsmanName){
 }
 function updateBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes , strikeRate){
     let batsmanFilePath = `./IPL/${teamName}/${batsmanName}.json`;
-    let batsmanFile = JSON.parse(fs.readFileSync(batsmanFilePath));  // hum batsmen ki file ko json se normal obj me convert karte hai taki hum us file ko update kar ske yani nyi inning push kar ske
-    let inning = {                                                   // iss uper wale step ki createBatsmanFile wale fxn me isiliye jrurat nhi pdi kyonki waha pe file 1st time create ki thi jo ki ek array thi naki koi json type ki
+    let batsmanFile = JSON.parse(fs.readFileSync(batsmanFilePath));
+    let inning = {
         Runs : runs , 
         Balls : balls , 
         Fours : fours , 
@@ -73,38 +67,39 @@ function updateBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes
     fs.writeFileSync( batsmanFilePath , JSON.stringify(batsmanFile) );
 }
 function createBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes , strikeRate){
-    let batsmanFilePath = `./IPL/${teamName}/${batsmanName}.json`; //path bnaya 
-    let batsmanFile = [];  //file bnayi array type ki b/c maybe ek hi bnde ne 1 se jyda match khele ho to uski 1 se jyda obj file bnegi ab use hum ek aaray me store kr lenge
-    let inning = {   // ek inning ka obj create kiya jisme key bnakar diff chije store kra li
+    let batsmanFilePath = `./IPL/${teamName}/${batsmanName}.json`;
+    let batsmanFile = [];
+    let inning = {
         Runs : runs , 
         Balls : balls , 
         Fours : fours , 
         Sixes : sixes ,
         StrikeRate : strikeRate
     }
-    batsmanFile.push(inning);  // inning ke obj ko file me push kar diya
-    fs.writeFileSync( batsmanFilePath , JSON.stringify(batsmanFile) );  //jo batsmen ki file hai jo ek array hai jisme obj pde hai. Ab jab tak hum usko stringify nhi kar dete tab tak uski values json me nhi dhikhegi for further check Notes
+    batsmanFile.push(inning);
+    fs.writeFileSync( batsmanFilePath , JSON.stringify(batsmanFile) );
 }
 function createTeamFolder(teamName){
     let teamFolderPath = `./IPL/${teamName}`;
     fs.mkdirSync(teamFolderPath);
 }
 function processDetails(teamName , batsmanName , runs , balls , fours , sixes , strikeRate){
-    let isTeamFolder = checkTeamFolder(teamName);  //check ki team ka folder pahle exist karta hai ki nhi
-    if(isTeamFolder){  // agr karta h to
-        let isBatsmanPresent = checkBatsmanFile(teamName , batsmanName); //check ki us batsmen ki file uske team ke folder me pahle se hai ki nhi
-        if(isBatsmanPresent){   
+    let isTeamFolder = checkTeamFolder(teamName);
+    if(isTeamFolder){
+        let isBatsmanPresent = checkBatsmanFile(teamName , batsmanName);
+        if(isBatsmanPresent){
             updateBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes , strikeRate);
         }
         else{
             createBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes , strikeRate);
         }
     }
-    else{  // agr nhi karta to
+    else{
         createTeamFolder(teamName);
         createBatsmanFile(teamName , batsmanName , runs , balls , fours , sixes , strikeRate);
     }
 }
+
 
 
 module.exports = getMatchDetails;
