e61e6afc5934c78656634c7a25c7e355920d2a2c
HardeepNain <hnspnain@gmail.com>
2023-02-25 23:48:24 +0530

    v1 org explained

--- allMatches.js
+++ allMatches.js
@@ -1,28 +1,27 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const getMatchDetails = require("./match");
+// const allMatchesLink = require("./homepage");  //#
 
 
-
-function getAllMatches(allMatchesLink){
-    request(allMatchesLink , function(err , res , data){
+function getAllMatches(allMatchesLink){  // yaha pe isko allMatchLink joki Homepage se mila hai pass hoga
+    request(allMatchesLink , function(err , res , data){  // ye hof hai to jaise hi isko data mila ye processData ko call laga dega
        processData(data);
     })
 }
 
 
-function processData(html){
+function processData(html){  //
     let myDocument = cheerio.load(html);
-    let allATags = myDocument('a[data-hover="Scorecard"]');
-    console.log(allATags.length);
-    //    { "0" : {aTag} , ......... "59" : {aTag} };
+    let allATags = myDocument('a[data-hover="Scorecard"]');  // selectors using values // jitne bhi matches us homepage pe the unke scorecard ke anker link aage
+    // console.log(allATags.length);
+    // { "0" : {aTag} , ......... "59" : {aTag} };
+
     for(let i=0 ; i<allATags.length ; i++){
-        let matchLink =  "https://www.espncricinfo.com" + myDocument(allATags[i]).attr("href");
-        // console.log(matchLink);
-        getMatchDetails(matchLink);
+        let matchLink =  "https://www.espncricinfo.com" + myDocument(allATags[i]).attr("href");  // yha se hume ek ek krke saare matches ke scorecard ke link milte jaayenge 
+        // console.log(matchLink);           
+        getMatchDetails(matchLink);                                                               // jise hum yha ek ek krke pass krte jaayenge.
     }   
 }
-
-
 
 module.exports = getAllMatches;
